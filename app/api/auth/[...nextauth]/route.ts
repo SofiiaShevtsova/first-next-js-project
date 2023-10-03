b94e4b40bc81f36869afd963b649a8d0b90c7f62
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2023-10-03 19:56:32 +0300

    do all for auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,19 +1,41 @@
-import NextAuth, {Profile} from "next-auth";
+import NextAuth, { Profile, Awaitable } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { connectToBD } from "@utils/database";
+import User from "@models/user";
 
 const handler = NextAuth({
-    providers: [GoogleProvider({ clientId: "", clientSecret: "" })],
-    callbacks: {
-  session: async ({ session }: any) => session,
-        signIn: async ({ profile }: { profile?: Profile | undefined }) => {
-      try {
-        await connectToBD()
-      } catch (error) {
-        
-      }
-  },
+  providers: [GoogleProvider({ clientId: "", clientSecret: "" })],
+  callbacks: {
+    session: async ({ session }: any) => {
+      const sessionUser = await User.findOne({ email: session.user.email });
+      session.user.id = sessionUser._id.toString();
+      return session;
+    },
+    signIn: ({
+      profile,
+    }: {
+      profile?: Profile | undefined;
+    }): Awaitable<string | boolean> => {
+      const connect = async () => {
+        try {
+          await connectToBD();
+          const userExists = await User.findOne({ email: profile?.email });
+          if (!userExists) {
+            await User.create({
+              email: profile?.email,
+              username: profile?.name?.replace(" ", "").toLowerCase(),
+              image: profile?.image,
+            });
+          }
+          return true;
+        } catch (error) {
+          console.log(error);
+          return false;
+        }
+      };
+      return connect();
+    },
   },
 });
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
---
+++ models/user.ts
@@ -0,0 +1,22 @@
+import { Schema, model, models } from "mongoose";
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    unique: [true, "Email already exists!"],
+    require: [true, "Email must exists!"],
+  },
+  username: {
+    type: String,
+    require: [true, "Name must exists!"],
+    min: 3,
+    max: 10,
+  },
+  image: {
+    type: String,
+  },
+});
+
+const User = models.User || model("User", UserSchema);
+
+export default User;
