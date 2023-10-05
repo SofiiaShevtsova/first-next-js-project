7ebee53302c529e81cf437b5e38b9580796f5658
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2023-10-05 20:50:48 +0300

    add search form

--- components/Feed/Feed.tsx
+++ components/Feed/Feed.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+"use client";
+
+import { useState, useEffect, ChangeEvent } from "react";
+
+import { PromptCard } from "@components/commons";
 
 export const Feed = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handlerSearch = (event: ChangeEvent) => {
+    const input = event.target as HTMLInputElement;
+    input && setSearchQuery(input.value);
+  };
+
   return (
-    <div>Feed</div>
-  )
-}
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search ..."
+          value={searchQuery}
+          onChange={handlerSearch}
+          className="search_input peer"
+        />
+      </form>
+    </section>
+  );
+};
