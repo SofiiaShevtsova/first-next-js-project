16c8354111a3e8943d6bfd8a22f491e34baf98bf
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2023-10-25 14:22:39 +0300

    fix session

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,19 @@
+"use client"
+
 import { ReactElement, ReactNode } from "react";
-import { useSession } from "next-auth/react";
 import { Navigation, Provider } from "@components/commons";
 import "@styles/global.css";
 
-export const metadata = {
-  title: "Promptopia",
-  description: "Discover and Share AI Prompts",
-};
-
 const RootLayout = ({ children }: { children: ReactNode }): ReactElement => {
-    const { data: session } = useSession();
 
   return (
     <html lang="en">
+      <head>
+        <title>Promptopia</title>
+        <meta name="description" content="Discover and Share AI Prompts"/>
+      </head>
       <body>
-        <Provider session={session}>
+        <Provider>
           <>
             <div className="main">
               <div className="gradient"></div>
--- components/Provider/Provider.tsx
+++ components/Provider/Provider.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from 'react'
 import { SessionProvider } from "next-auth/react";
 
 
-export const Provider = ({ children, session }: { children: ReactNode;  session: any}) => {
+export const Provider = ({ children, session }: { children: ReactNode;  session?: any}) => {
   return (
     <SessionProvider session={session}>
       {children}
